c1d8a854e30b8a4084535b87b9758459b1a05536
Storozhilov <gruppa203git@mail.ru>
2023-12-08 11:58:28 +0300

    Public

---
+++ Сессия 1/WpStozohilovEgor/WpStozohilovEgor/Страницы/Логин.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpStozohilovEgor.МодельБД;
+
+namespace WpStozohilovEgor.Страницы
+{
+    /// <summary>
+    /// Логика взаимодействия для Логин.xaml
+    /// </summary>
+    public partial class Логин : Page
+    {
+        public Логин()
+        {
+            InitializeComponent();
+        }
+        private void BtnToComeIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = bdStorozhilovEntities.GetContext().Users.FirstOrDefault(x => x.Логин == TextBoxLogin.Text && x.Пароль == PasswordBoxPassword.Password);
+                if (userObj == null)
+                {
+                    MessageBox.Show("Пользователь с таким логином и паролем не найден!", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+                else
+                {
+                    MessageBox.Show($"Добро пожаловать {userObj.Имя_Отчество}");
+                    GetFrame.MainFrame.Navigate(new СписокТоваров());
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка:" + Ex.Message.ToString(), "Критическая работа приложения", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+        }
+    }
+}
---
+++ Сессия 1/WpStozohilovEgor/WpStozohilovEgor/Страницы/СписокТоваров.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpStozohilovEgor.МодельБД;
+namespace WpStozohilovEgor.Страницы
+{
+    /// <summary>
+    /// Логика взаимодействия для СписокТоваров.xaml
+    /// </summary>
+    public partial class СписокТоваров : Page
+    {
+        public СписокТоваров()
+        {
+            InitializeComponent();
+            SortList.SelectedIndex = 0;
+            FilterList.SelectedIndex = 0;
+        }
+        private void Page_IsVisibleChange(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                bdStorozhilovEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGProducts.ItemsSource = bdStorozhilovEntities.GetContext().Products.ToList();
+                Application.Current.MainWindow.Title = $"Концелярия {DGProducts.Items.Count} из {bdStorozhilovEntities.GetContext().Products.Count()}";
+            }
+            
+        }
+
+        private void BtnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilterList.Text == "Все диапазоны")
+            {
+                DGProducts.ItemsSource = bdStorozhilovEntities.GetContext().Products.ToList();
+            }
+            else if (FilterList.Text == "0-9,99%")
+            {
+                DGProducts.ItemsSource = bdStorozhilovEntities.GetContext().Products.Where(x => x.Размер_максимально_возможной_скидки < 10).ToList();
+            }
+            else if (FilterList.Text == "10-14,99%")
+            {
+                DGProducts.ItemsSource = bdStorozhilovEntities.GetContext().Products.Where(x => x.Размер_максимально_возможной_скидки >= 10 && x.Размер_максимально_возможной_скидки < 15).ToList();
+            }
+            else if (FilterList.Text == "15% и более")
+            {
+                DGProducts.ItemsSource = bdStorozhilovEntities.GetContext().Products.Where(x => x.Размер_максимально_возможной_скидки >= 15).ToList();
+            }
+            Application.Current.MainWindow.Title = $"Концелярия {DGProducts.Items.Count} из {bdStorozhilovEntities.GetContext().Products.Count()}";
+        }
+        private void BtnSort_Click(object sender, RoutedEventArgs e)
+        {
+            if (SortList.Text == "По возрастанию")
+            {
+                DGProducts.Items.SortDescriptions.Clear();
+                DGProducts.Items.SortDescriptions.Add(new SortDescription("Стоимость", ListSortDirection.Ascending));
+            }
+            if (SortList.Text == "По убыванию")
+            {
+                DGProducts.Items.SortDescriptions.Clear();
+                DGProducts.Items.SortDescriptions.Add(new SortDescription("Стоимость", ListSortDirection.Descending));
+            }
+            if (SortList.Text == "Все значения")
+            {
+                DGProducts.Items.SortDescriptions.Clear();
+            }
+        }
+
+        private void BtnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            GetFrame.MainFrame.GoBack();
+        }
+    }
+}
